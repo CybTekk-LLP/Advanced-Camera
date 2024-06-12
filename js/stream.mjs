c01d0756ead6c6f5da535bda5ece1562e93d0949
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-06-13 02:15:36 +0530

    fix zoom

--- js/stream.mjs
+++ js/stream.mjs
@@ -47,7 +47,7 @@ export const streamWebCamVideo = (isFrontCamera = true) => {
       input.oninput = function (event) {
         track.applyConstraints({ advanced: [{ zoom: event.target.value }] });
         document.querySelector(".slider").style.left =
-          (input.value * 120) / capabilities.zoom.max + "px";
+          ((input.value - 0.5) * 125) / capabilities.zoom.max + "px";
       };
       input.hidden = false;
       document.querySelector(".range-container").style.visibility = "visible";
